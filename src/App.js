582000d92b18198949698e08e0d4d5d950884016
Winnie Chow <WinnieChow606@outlook.com>
2023-10-11 22:59:06 +0100

    Paritally implemented homepage

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import {
   Routes,
   Route,
-  Link
 } from "react-router-dom";
 
 //page imports
@@ -12,35 +11,30 @@ import Skills from './routes/skills';
 import Projects from './routes/projects';
 import Contact from './routes/contact';
 
+//react-bootstrap imports
+import 'bootstrap/dist/css/bootstrap.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
 export default function App() {
   return (
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/skills">Skills</Link>
-            </li>
-            <li>
-              <Link to="/projects">Projects</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact Me!</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-
-      </div>
+    <>
+      <Navbar bg="dark" data-bs-theme="dark" sticky='top'>
+        <Container>
+          <Navbar.Brand href="#">Winnie Chow</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#contact">Contact Me</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes></>
   );
 }
--- src/routes/home.js
+++ src/routes/home.js
@@ -1,14 +1,19 @@
 import '../App.css';
+import { Image } from 'react-bootstrap';
 
 function Home() {
   return (
-    <div className="App">
+    <body>
+          <div className="App">
       <header className="App-header">
+        <Image src='https://as1.ftcdn.net/v2/jpg/03/39/45/96/1000_F_339459697_XAFacNQmwnvJRqe1Fe9VOptPWMUxlZP8.jpg' roundedCircle width={250} height={250}/>
         <p>
-          Hello World! This is the home page :)
+          A nerd who somehow got a hold of a computer
         </p>
+        <p style={{fontSize:17}}>Hello, I'm Winnie! I am a software engineer who specialises in creating modern web applications utilising REST APIs.</p>
       </header>
     </div>
+    </body>
   );
 }
 
