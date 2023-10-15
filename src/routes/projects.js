47e8d4e2c89d54aecac57bcb9d5e7243f8db6789
Winnie Chow <WinnieChow606@outlook.com>
2023-10-15 01:17:09 +0100

    Updated Projects Page

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -1,13 +1,31 @@
+import { Badge, Card, Col, Row, Stack } from 'react-bootstrap';
 import '../App.css';
 import projectsJSON from '../utils/projectsJSON';
 
+
 function Projects() {
+
+  const projectsCardList = projectsJSON.map(project =>
+    <Col>
+      <Card bg='dark' text='white'>
+        <Card.Img variant="top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png"  />
+        <Card.Body>
+        <Card.Header>{project.projectTitle}</Card.Header>
+        <Card.Text>{project.projectDescShort}</Card.Text>
+        </Card.Body>
+        <Stack direction="horizontal" gap={2} style={{display: "flex", justifyContent: "center"}}>{project.projectSkills.map(skill => 
+          <Badge bg="secondary">{skill}</Badge>)}
+        </Stack>
+        <Card.Link>Learn more...</Card.Link>
+      </Card>
+      <br/>
+    </Col>);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          {projectsJSON[0].projectTitle}
-        </p>
+      <header className="App-header" style={{padding: 10}}>
+        <Row xs={1} md={3}>{projectsCardList}</Row>
       </header>
     </div>
   );
--- src/utils/projectsJSON.js
+++ src/utils/projectsJSON.js
@@ -5,10 +5,16 @@
 - projectSkills: An array of technical skills used during the project
 - projectDescLong: The project description; the longer version
 - projectMedia: An array of relevant media (pictures/videos) related to the project
+- projectDate: The date when the project occured
+- projectLinks: A JSON Array containing all relevant project links (GitHub, Website)
 */
 
 let projectsJSON = [
-    {projectID : 0, projectTitle: "WhatsThat App", projectDescShort: "A clone of the popular app WhatsApp I made during my final year of Uni.", projectSkills: ["React Native"]}
+    {projectID : 0, projectTitle: "WhatsThat App", projectDescShort: "A clone of the popular app WhatsApp I made during my final year of Uni.", projectSkills: ["React Native"]},
+    {projectID : 1, projectTitle: "Pokédex", projectDescShort: "A simple pokémon inspired website displaying pokémon data utilising the pokéAPI.", projectSkills: ["React", "HTML","CSS", "JS"]},
+    {projectID : 2, projectTitle: "Films API", projectDescShort: "A simple API displaying Film data in XML, JSON and String format.", projectSkills: ["Java Servlets"]},
+    {projectID : 3, projectTitle: "SQL Detective", projectDescShort: "A Simple web game designed to teach SQL using gamification principles", projectSkills: ["HTML", "CSS", "JavaScript", "GitHub Pages"]},
+    {projectID : 4, projectTitle: "Online Dictionary", projectDescShort: "A quick and simple dictionary Website made for the GreatUniHack in 2022", projectSkills: ["Velvo Wix"]}
 ]
 
 export default projectsJSON;
