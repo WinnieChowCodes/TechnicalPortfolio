c5fb04a80302da4aa5a210e0e937d787bbfef38f
Winnie Chow <WinnieChow606@outlook.com>
2023-10-17 19:58:55 +0100

    Initial Upload of Contact Page
    (Continue work in node.js server)

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -1,5 +1,5 @@
 import '../App.css';
-
+import { Form } from 'react-bootstrap';
 function Contact() {
   return (
     <div className="App">
@@ -7,7 +7,28 @@ function Contact() {
         <p>
           Hello World! This is the Contact :)
         </p>
+        <ul>
+          <li>Offer Email and Phone number</li>
+          <li>Social Media Links</li>
+          <li>Contact Form (email, Name, Message)</li>
+        </ul>
       </header>
+      <div>
+        <Form>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type='email'/>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Name</Form.Label>
+            <Form.Control/>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Message</Form.Label>
+            <Form.Control as="textarea" rows={3} />
+          </Form.Group>
+        </Form>
+      </div>
     </div>
   );
 }
